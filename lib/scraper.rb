f7b270ff77cc6e58e73bd92bc8e073b05f414af4
Greg Morgan <gregm9999@gmail.com>
2018-11-15 00:28:56 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -30,6 +30,16 @@ class Scraper
   end
 end
 
+def print_courses
+  self.make_courses
+  Course.all.each do |course|
+    if course.title
+      puts "Title: #{course.title}"
+      puts "  Schedule: #{course.schedule}"
+      puts "  Description: #{course.description}"
+    end
+  end
+
 end
 
 Scraper.new.print_courses
